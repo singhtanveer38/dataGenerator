ee1b883bda059af1de2159346708ae174ff63307
singhtanveer38 <singhtanveer38@yahoo.com>
2021-11-16 02:54:15 +0530

    sin wave started

--- dataGenerator/curves.py
+++ dataGenerator/curves.py
@@ -1,9 +1,32 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class sin(object):
 
-	def __init__(self, val):
-		self.val = val
+	def __init__(self, amplitude, phase, minValue, maxValue, steps):
+		self.amplitude = amplitude
+		self.phase = phase
+		self.minValue = minValue
+		self.maxValue = maxValue
+		self.steps = steps
+		# self.phaseMultiplier = phaseMultiplier
+		# self.phaseOffset = phaseOffset
+		# self.valueOffset = valueOffset
 
 	def calculate(self):
-		return np.sin(self.val)
+		self.x = np.arange(self.minValue, self.maxValue, self.steps)
+		self.y = np.sin(self.x*self.phase) + self.amplitude
+
+	def plot(self):
+		plt.scatter(self.x, self.y)
+		plt.show()
+
+
+
+
+
+data = sin(amplitude=0, phase=2, minValue=0, maxValue=10, steps=0.1)
+data.calculate()
+print(data.x)
+print(data.y)
+data.plot()
